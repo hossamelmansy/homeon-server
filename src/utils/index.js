831aee6a45bc20b35a092dafff27069e81d863bd
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2019-07-20 23:52:01 +0200

    adding throwError function and validator for inputs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,32 @@
-const mongoose = require("mongoose");
 const path = require("path");
 const fs = require("fs");
 const pick = require("lodash/pick");
+const {
+  AuthenticationError,
+  UserInputError,
+  ForbiddenError,
+  ApolloError
+} = require("apollo-server");
+
+const ERRORS = {
+  AUTHENTICATION: "authentication_error",
+  USERINPUT: "user_input_error",
+  FORBIDDEN: "forbidden_error",
+  CUSTOM: "custom_error"
+};
 
 module.exports = {
   loadGQLfile,
-  isValidId,
-  formatError
+  formatError,
+  ERRORS,
+  throwError
 };
 
-// #########################
+// ###########################################
 
 function loadGQLfile(type) {
   var filePath = path.join(__dirname, "../api", type);
   return fs.readFileSync(filePath, "utf-8");
-}
-
-function isValidId(id = "") {
-  return mongoose.Types.ObjectId.isValid(id);
 }
 
 function formatError(err) {
@@ -26,4 +35,17 @@ function formatError(err) {
   }
 
   return err;
+}
+
+function throwError(type = ERRORS.CUSTOM, message = "Error", code = "ERROR") {
+  switch (type) {
+    case ERRORS.AUTHENTICATION:
+      throw new AuthenticationError("Authentication required!");
+    case ERRORS.FORBIDDEN:
+      throw new ForbiddenError("Forbidden!");
+    case ERRORS.USERINPUT:
+      throw new UserInputError(message);
+    default:
+      throw new ApolloError(message, code);
+  }
 }
---
+++ src/utils/validator.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ERRORS, throwError } = require("./index");
+
+module.exports = {
+  isValidId,
+  isValidEmail,
+  isValidPhone,
+  isValidPassword
+};
+
+// ####################################
+
+function isValidId(value = "", options = { error: false }) {
+  var { error = false } = options;
+  var valid = mongoose.Types.ObjectId.isValid(value.trim());
+
+  return error && !valid ? throwError(ERRORS.USERINPUT, "Invalid id") : valid;
+}
+
+function isValidEmail(value = "", options = { error: false }) {
+  var { error = false } = options;
+  var valid = validator.isEmail(value.trim());
+
+  return error && !valid
+    ? throwError(ERRORS.USERINPUT, "Invalid email")
+    : valid;
+}
+
+function isValidPhone(value = "", options = { locale: "ar-EG", error: false }) {
+  var { locale = "ar-EG", error = false } = options;
+  var valid = validator.isMobilePhone(value.trim(), locale);
+
+  return error && !valid
+    ? throwError(ERRORS.USERINPUT, "Invalid phone")
+    : valid;
+}
+
+function isValidPassword(value = "", options = { error: false }) {
+  var { error = false } = options;
+  var valid = !validator.isEmpty(value);
+
+  return error && !valid
+    ? throwError(ERRORS.USERINPUT, "Invalid password")
+    : valid;
+}
